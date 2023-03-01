1b73e216555ad9e44a58b6ba30ed833599c489ed
eugeniusms <eugeniusms@gmail.com>
2023-03-01 14:08:29 +0700

    #4 Adapter Pattern Done

--- src/main/java/id/ac/ui/cs/advprog/tutorial3/Belanjaa/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/tutorial3/Belanjaa/service/PaymentServiceImpl.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.tutorial3.Belanjaa.service;
 
+import id.ac.ui.cs.advprog.tutorial3.Belanjaa.core.Coupon;
 import id.ac.ui.cs.advprog.tutorial3.Belanjaa.core.Item;
 import id.ac.ui.cs.advprog.tutorial3.Belanjaa.core.adapter.CouponAdapter;
 import id.ac.ui.cs.advprog.tutorial3.Belanjaa.core.adapter.GiftAdapter;
@@ -33,6 +34,28 @@ public class PaymentServiceImpl implements PaymentService{
     @Override
     public String makePayment(String itemName, String paymentCode) {
         // TODO: implement method, return value below if no payment is made
+        // Mendapatkan item sesuai nama itemnya
+        Item item = itemRepository.getItem(itemName);
+
+        if (item == null) {
+            return "Payment log is empty";
+        }
+
+        Boolean isCouponExist = paymentRepository.contains(paymentCode);
+        Boolean isGiftExist = giftRepository.contains(paymentCode);
+        Boolean isPaymentExist = giftRepository.contains(paymentCode);
+
+        if (isCouponExist) {
+            CouponAdapter couponAdapter = new CouponAdapter(couponRepository.getCoupon(paymentCode));
+            return couponAdapter.pay(item);
+        }
+        if (isGiftExist) {
+            GiftAdapter giftAdapter = new GiftAdapter(giftRepository.getGift(paymentCode));
+            return giftAdapter.pay(item);
+        }
+        if (isPaymentExist) {
+            return paymentRepository.getPayment(paymentCode).pay(item);
+        }
 
         return "Payment log is empty";
     }
